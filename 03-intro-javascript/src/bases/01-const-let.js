9fac9139d70e19c51bd43af181e1035997b24dfd
quinterodiego <d86webs@gmail.com>
2022-07-03 23:00:11 -0300

    bases 05

---
+++ 03-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// variables y constantes
+
+const nombre = 'Diego';
+const apellido = 'Quintero';
+
+let valorDado = 5;
+valorDado = 4
+
+console.log(nombre, apellido, valorDado)
+
+if(true){
+    const nombre = 'Nicolas';
+    let valorDado = 6;
+    console.log(nombre)
+}
+
+console.log(valorDado)
