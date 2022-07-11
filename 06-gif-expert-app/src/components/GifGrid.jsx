6614680a9dc1b127a7f260ebca60f12ff12decb9
Diego Quintero <d86webs@gmail.com>
2022-07-11 18:03:40 -0300

    asd

--- 06-gif-expert-app/src/components/GifGrid.jsx
+++ 06-gif-expert-app/src/components/GifGrid.jsx
@@ -1,4 +1,21 @@
 export const GifGrid = ({ category }) => {
+
+    const getGifs = async () => {
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=GI0p3SZ9v7Fv7Z5PFF62iCy5tXxt3AhI&q=${category}&limit=20`;
+        const resp = await fetch( url );
+        const { data } = await resp.json();
+        
+        const gifs = data.map(img => ({
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }));
+
+        console.table(gifs);
+    }
+
+    getGifs();
+
     return (
         <>
             <h3>{ category }</h3>
