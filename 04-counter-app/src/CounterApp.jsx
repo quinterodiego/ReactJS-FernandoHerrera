37a1945c9afafc640d6230d07e8de1dd1715d6d0
quinterodiego <66433342+quinterodiego@users.noreply.github.com>
2022-10-19 23:50:32 -0300

    asd

--- 04-counter-app/src/CounterApp.jsx
+++ 04-counter-app/src/CounterApp.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types';
 
-const handleAdd = () => console.log('+1');
-
 export const CounterApp = ({ value }) => {
+
+    const [ counter, setCounter ] = useState( value );
+
+    const handleAdd = () => {
+        setCounter( counter + 1 );
+    };
+
+    const handleSubtract = () => {
+        setCounter( counter - 1 );
+    };
+
+    const handleReset = () => {
+        setCounter( value );
+    };
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{ value }</h2>
+            <h2>{ counter }</h2>
 
+            <button onClick={ handleSubtract }>-1</button>
+            <button onClick={ handleReset }>Reset</button>
             <button onClick={ handleAdd }>+1</button>
         </>
     )
