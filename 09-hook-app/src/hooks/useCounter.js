ffb8d2b6487665132d861726efe95bfadedb1ac5
quinterodiego <d86webs@gmail.com>
2023-01-02 23:19:49 -0300

    asd

---
+++ 09-hook-app/src/hooks/useCounter.js
@@ -0,0 +1,10 @@
+import {useState} from 'react'
+
+export const useCounter = (initialValue = 10) => {
+    
+    const [counter, setcounter] = useState(initialValue);
+    
+    return {
+        counter,
+    }
+}
