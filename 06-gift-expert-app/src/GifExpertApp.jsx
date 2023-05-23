0fd4fe284b47f41ae6cf9b4e83ee46dddea56216
quinterodiego <d86webs@gmail.com>
2023-05-23 00:01:45 -0300

    asd

--- 06-gift-expert-app/src/GifExpertApp.jsx
+++ 06-gift-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
+import { AddCategory } from './components/AddCategory';
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['cities'])
+
     return (
         <>
             <h1>GifExpertApp</h1>
+
+            <AddCategory setCategories={setCategories} categories={categories} />
+
+            <ol>
+                { 
+                    categories.map(category => {
+                        return <li key={category}>{category}</li>
+                    }) 
+                }
+            </ol>
         </>
     )
 }
