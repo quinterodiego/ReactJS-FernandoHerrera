b68015d79ecf7dc626ab79cd14aa147a71e934f9
quinterodiego <d86webs@gmail.com>
2023-08-17 23:32:51 -0300

    asd

--- 18-toolkit-redux/src/main.jsx
+++ 18-toolkit-redux/src/main.jsx
@@ -5,11 +5,15 @@ import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
 import { store } from './store/store'
+import { PokemonApp } from './PokemonApp'
+import { TodoApp } from './TodoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>
 )
