49119f43a4b903f1f356ce6605b7ad115b220a15
quinterodiego <d86webs@gmail.com>
2023-08-29 18:09:10 -0300

    asd

--- 09-hook-app/src/02-useEffect/Message.jsx
+++ 09-hook-app/src/02-useEffect/Message.jsx
@@ -4,8 +4,15 @@ export const Message = () => {
 
   useEffect(() => {
     console.log('Mesagge Mounted')
+    const onMouseMove = ({ x, y }) => {
+      const coords = { x, y }
+      console.log(coords)
+    }
+
+    window.addEventListener('mousemove', onMouseMove)
   
     return () => {
+      window.removeEventListener('mousemove', onMouseMove)
       console.log('Message Unmounted')
     }
   }, [])
