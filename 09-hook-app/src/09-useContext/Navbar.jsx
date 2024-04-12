c04cba152684b47d423fb1fb97ceadf768910cd5
quinterodiego <d86webs@gmail.com>
2024-04-12 17:49:59 -0300

    asd+

--- 09-hook-app/src/09-useContext/Navbar.jsx
+++ 09-hook-app/src/09-useContext/Navbar.jsx
@@ -2,12 +2,18 @@ import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <>
-      <nav>
-        <Link to='/'>HOME</Link>
-        <Link to='/login'>LOGIN</Link>
-        <Link to='/about'>ABOUT</Link>
-      </nav>
-    </>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">useContext</Link>
+          <ul className="navbar-nav d-flex flex-row gap-3">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+          </ul>
+      </div>
+    </nav>
   )
 }
