866a9773145f6913ac82a34e3d7a25506909de40
quinterodiego <d86webs@gmail.com>
2024-06-28 18:07:53 -0300

    asd

--- 14-heroes-app/src/auth/context/AuthProvider.jsx
+++ 14-heroes-app/src/auth/context/AuthProvider.jsx
@@ -1,11 +1,29 @@
+import { useReducer } from "react"
 import { AuthContext } from "./AuthContext"
+import { authReducer } from "./AuthReducer"
+import { types } from "../types/types"
+
+const initialState = {
+  logged: false
+}
 
 export const AuthProvider = ({ children }) => {
 
-  
+  const [state, dispatch] = useReducer(authReducer, initialState)
+
+  const login = async (name = '') => {
+    const action = {
+      type: types.login,
+      payload: {
+        id: 'ABC',
+        name: name
+      }
+    }
+    dispatch(action)
+  }
 
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{ login }}>
       {children}
     </AuthContext.Provider>
   )
