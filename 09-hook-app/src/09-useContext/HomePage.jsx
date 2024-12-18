3848cb11a13ce4c7dff06c93c4ecf14c0160c4ec
Diego Quintero <d86webs@gmail.com>
2024-12-17 23:18:59 -0300

    asd

--- 09-hook-app/src/09-useContext/HomePage.jsx
+++
@@ -1,8 +0,0 @@
-export const HomePage = () => {
-  return (
-    <>
-      <h1>HomePage</h1>
-      <hr />
-    </>
-  )
-}
--- 14-heroes-app/src/heroes/pages/index.js
+++
@@ -1,4 +0,0 @@
-export * from './DCPage'
-export * from './MarvelPage'
-export * from './SearchPage'
-export * from './HeroPage'
