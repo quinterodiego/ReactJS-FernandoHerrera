c79c7d31160f18dd298df73bcb5a03fe3e9b280d
Diego Quintero <d86webs@gmail.com>
2025-10-17 00:25:25 -0300

    asd

--- 01-reforzamiento/src/bases/03-object-literals.ts
+++ 01-reforzamiento/src/bases/03-object-literals.ts
@@ -1,5 +1,25 @@
-const person = {
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+  address: Address;
+}
+
+interface Address {
+  postalCode: string;
+  city: string;
+  country: string;
+}
+
+const ironman: Person = {
   firstName: 'Tony',
   lastName: 'Stark',
-  age: 45
+  age: 45,
+  address: {
+    postalCode: 'ABC123',
+    city: 'New York',
+    country: 'USA'
+  }
 }
+
+console.log( ironman );
--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css';
 // import './bases/01-const-let';
 // import './bases/02-template-string';
-import './bases/03-object-literals';
+// import './bases/03-object-literals';
+import './bases/04-arrays';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
